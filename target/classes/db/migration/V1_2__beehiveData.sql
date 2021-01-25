e34a24e212ab319675526e82e124654d1295b1cc
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2021-01-25 17:54:28 +0100

    Added jar file

--- target/classes/db/migration/V1_2__beehiveData.sql
+++ target/classes/db/migration/V1_2__beehiveData.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS `beehive_data` (
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`data_id`),
   KEY `FK_sensor_id` (`sensor_id`),
-  CONSTRAINT `sensor_id` FOREIGN KEY (`sensor_id`) REFERENCES `beehive` (`sensor_id`) ON DELETE NO ACTION ON UPDATE CASCADE
+  CONSTRAINT `sensor_id` FOREIGN KEY (`sensor_id`) REFERENCES `beehive` (`sensor_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=21492 DEFAULT CHARSET=utf8;
